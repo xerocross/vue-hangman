1f3db4bc0a0ff46c93237175a69c2f882410a18c
Adam Cross <adamfgcross@gmail.com>
2023-05-31 16:27:24 -0400

    correct text in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+express.static.mime.types['js'] = 'application/javascript';
 app.use(express.static('public'))
 app.use('/dist', express.static('dist'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Vue Hangman listening on port ${port}!`))
